e93b1ba52f6f90d6ee7149546732140c800be207
user <ravneets47@gmail.com>
2019-05-21 19:42:11 -0400

    Directories created

---
+++ src/component/Sortingbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return(
+        <div>
+            Sort by location, Distance, Amount
+        </div>
+    );
+}
+
+export default SearchBar;
--- src/container/SearchDestination.js
+++ src/container/SearchDestination.js
@@ -6,26 +6,27 @@ class SearchDestination extends Component {
       query: '',
     }
    
-    handleInputChange = () => {
+    handleInputChange = (event) => {
       this.setState({
-        query: this.search.value
+        query: event.target.value
       })
     }
    
     render() {
       return (
+
         <div style={{
           backgroundImage: "url(" +  Travel  + ")", 
           backgroundColor : '#cccccc', 
           height: "540px", 
           display : "flex", 
+          flexDirection: 'column',
           justifyContent : 'center', 
-          flexFlow: 'row wrap',
-          alignItems:  'stretch'}}>
+          alignItems:  'center'}}>
+          
           <h1 style={{
               color: "#5ba16f",
               fontSize: '280%',
-              flex: "1 100%", 
               padding: "10px", 
               textAlign: 'center'}}>
             <bold>Travelling</bold> Made Easy
@@ -35,9 +36,11 @@ class SearchDestination extends Component {
             style={{
                 height: "40px", 
                 width: "400px", 
-                flex: "1"}}
+                flex: "1",
+                fontSize: "18px"}}
             placeholder="Search for..."
-            ref={input => this.search = input}
+            value = {this.state.query}
+            // ref={input => this.search = input}
             onChange={this.handleInputChange}
           />
         </form>
@@ -47,94 +50,4 @@ class SearchDestination extends Component {
    }
    
    export default SearchDestination;
-   
-
-//    import React, { Component }  from 'react';
-//    import { withStyles } from '@material-ui/core/styles';
-//    import Travel from '../assest/travel.jpg';
-   
-//    const styles = theme => ({
-//        container: {
-//            backgroundImage: "url(" +  Travel  + ")", 
-//            backgroundColor : '#cccccc', 
-//            height: 500, 
-//            display : "flex", 
-//            justifyContent : 'center', 
-//            alignItems:  'center'
-//        }
-//    });
-   
-   
-//    class SearchDestination extends Component {
-       
-//        state = {
-//          query: '',
-//        }
-      
-//        handleInputChange = () => {
-//          this.setState({
-//            query: this.search.value
-//          })
-//        }
-      
-//        render(props) {
-//        const { classes } = props;
-//          return (
-//            <div className={classes.container}>
-//              <h1>h</h1>
-//                <form>
-//                <input
-//                    style={{height: "40px", width: "400px"}}
-//                    placeholder="Search for..."
-//                    ref={input => this.search = input}
-//                    onChange={this.handleInputChange}
-//                />
-//                </form>
-//            </div>
-//          )
-//        }
-//       }
-      
-//       export default withStyles(styles)(SearchDestination);
-      
-
-
-
-
-
-
-
-
-// import React, { Component }  from 'react';
-// import SearchBar from '../component/SearchBar';
-// import Travel from '../assest/travel.jpg';
-
-// class SearchDestination extends Component {
-//     state = {
-//       query: null,
-//     }
-   
-//     handleInputChange = () => {
-//       this.setState({
-//         query: this.search.value
-//       })
-//     }
-   
-//     render() {
-//         console.log(this.state.query);
-//       return (
-//         <div style={{
-//           backgroundImage: "url(" +  Travel  + ")", 
-//           backgroundColor : '#cccccc', 
-//           height: 500, 
-//           display : "flex", 
-//           justifyContent : 'center', 
-//           alignItems:  'center'}}>
-//           <SearchBar triggerParentUpdate={this.handleInputChange}/>
-//         </div>
-//       )
-//     }
-//    }
-   
-//    export default SearchDestination;
    
