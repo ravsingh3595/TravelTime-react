0babdb6c8e04c35bb498c0003e23acd9c32038be
user <ravneets47@gmail.com>
2019-06-03 21:30:37 -0400

    Gallery page for single month created

--- src/Pages/PhotoGallery.js
+++ src/Pages/PhotoGallery.js
@@ -1,16 +1,68 @@
-import React from 'react';
-import Gallery from '../component/Gallery';
 
-class PhotoGallery extends React.Component {
-  render() {
-    return (
-      <div className="row">
-        <div className="col-md-4 col-md-offset-4">
-          <Gallery/>
-        </div>
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import Data from '../dummyData';
+import PropTypes from 'prop-types';
+
+const useStyles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    alignItems: 'center',
+    // backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+});
+
+function PhotoGallery() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div style={{width: "500px", maxHeight: "450px"}}>
+      <GridList cellHeight={200} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+          <ListSubheader component="div">Month Name</ListSubheader>
+        </GridListTile>
+        {Data.map(tile => (
+          <GridListTile key={tile.title}>
+            <img src={tile.url} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              subtitle={<span>by: {tile.author}</span>}
+              actionIcon={
+                <IconButton 
+                  aria-label={`info about ${tile.title}`}
+                  // className={classes.icon}
+                  color= "secondary">
+                  <InfoIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default PhotoGallery;
+PhotoGallery.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(useStyles)(PhotoGallery);
--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import { Route, IndexRoute, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 // import App from '../App'
 import Home from '../Pages/Home';
 import LoginPage from '../Pages/Login';
 import SignupPage from '../Pages/SignupPage'
-import SearchPage from '../Pages/SearchPage';
+// import SearchPage from '../Pages/SearchPage';
 import Itinerary from '../Pages/Itinerary';
-import Profile from "../Pages/Profile"
-
-// import App from './components/App';
-// import Greetings from './components/Greetings';
-// import SignupPage from './components/signup/SignupPage';
-// import LoginPage from './components/login/LoginPage';
-// import NewEventPage from './components/events/NewEventPage';
-
-// import requireAuth from './utils/requireAuth';
+import Profile from "../Pages/Profile";
+import PhotoGallery from "../Pages/PhotoGallery";
 
 export const  Router = () => {
-  return( <BrowserRouter>
-      <Route exact={true} path="/" component={Home}/>
-        <Route path="/signup" component={SignupPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/itinerary" component={Itinerary} />
-        <Route path = "/profile" component = {Profile} />
-      </BrowserRouter>)
- 
+  return( 
+  <BrowserRouter>
+    <Route exact={true} path="/" component={Home}/>
+    <Route path="/signup" component={SignupPage} />
+    <Route path="/login" component={LoginPage} />
+    <Route path="/itinerary" component={Itinerary} />
+    <Route path = "/profile" component = {Profile} />
+    <Route path = "/gallery" component = {PhotoGallery} />
+  </BrowserRouter>
+  )
 }
   
 
