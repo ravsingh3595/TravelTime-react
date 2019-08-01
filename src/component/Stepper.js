de85bbb5d4860e6ccbebd8822ebe34f2417bc572
Jaskeerat Singh Bhatia <jaskeerat.rsb@gmail.com>
2019-08-01 01:43:07 -0400

    feat : adds booking details tp redux store

--- src/component/Stepper.js
+++ src/component/Stepper.js
@@ -1,77 +1,78 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { itineraryData } from '../dummyData.js';
-import fire from '../Firebase/Firebase';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { itineraryData } from "../dummyData.js";
+import fire from "../Firebase/Firebase";
 
-import GettingDetails from '../component/GettingDetails';
+import GettingDetails from "../component/GettingDetails";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
+    width: "100%"
   },
   backButton: {
-    marginRight: theme.spacing(1),
+    marginRight: theme.spacing(1)
   },
   instructions: {
     // padding: "50",
     marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
+    marginBottom: theme.spacing(1)
   },
-  article:{
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%'
+  article: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%"
   },
-  aside1:{
+  aside1: {
     padding: 30,
     flex: 7,
-    borderRight: '6px solid black',
+    borderRight: "6px solid black"
   },
-  aside2:{
+  aside2: {
     flex: 2,
-    padding: 30,
-    
-    
+    padding: 30
   },
-  destination:{
-    border: '2px solid black',
-    padding: 20,
-
-  },
-
+  destination: {
+    border: "2px solid black",
+    padding: 20
+  }
 }));
 
 function getSteps() {
-  return ['Provide all the information of the travellers', 'Confirming the details', 'Pay and Confirm'];
+  return [
+    "Provide all the information of the travellers",
+    "Confirming the details",
+    "Pay and Confirm"
+  ];
 }
 
-function getStepContent(stepIndex) {
+function getStepContent(stepIndex, isNextClicked) {
   switch (stepIndex) {
     case 0:
       return (
         <div>
           {/* {this.setBookingId(shortid.generate())}
           {ref.push(this.bookingId)} */}
-          <GettingDetails/>
+          <GettingDetails isNextClicked={isNextClicked} />
         </div>
-        );
+      );
     case 1:
-      return 'Please confirm the given information';
+      return "Please confirm the given information";
     case 2:
-      return 'Pay and its all done';
+      return "Pay and its all done";
     default:
-      return 'Unknown stepIndex';
+      return "Unknown stepIndex";
   }
 }
 
 export default function HorizontalLabelPositionBelowStepper() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
+  const [isNextClicked, setisNextClicked] = React.useState(false);
   // const [date, setDate] = React.useState(new Date());
   // const [bookingId, setBookingId] = React.useState(0);
 
@@ -79,6 +80,7 @@ export default function HorizontalLabelPositionBelowStepper() {
 
   function handleNext() {
     setActiveStep(prevActiveStep => prevActiveStep + 1);
+    setisNextClicked(true);
   }
 
   function handleBack() {
@@ -89,10 +91,14 @@ export default function HorizontalLabelPositionBelowStepper() {
     setActiveStep(0);
   }
 
+  console.log("PAPAPA", isNextClicked);
   return (
-    
     <div className={classes.root}>
-      <Stepper activeStep={activeStep} alternativeLabel style={{backgroundColor: "#DBFFE5"}}>
+      <Stepper
+        activeStep={activeStep}
+        alternativeLabel
+        style={{ backgroundColor: "#DBFFE5" }}
+      >
         {steps.map(label => (
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
@@ -102,84 +108,66 @@ export default function HorizontalLabelPositionBelowStepper() {
       <div>
         {activeStep === steps.length ? (
           <div>
-            <Typography className={classes.instructions}>All steps completed</Typography>
+            <Typography className={classes.instructions}>
+              All steps completed
+            </Typography>
             <Button onClick={handleReset}>Reset</Button>
           </div>
         ) : (
           <div>
-
             <section className={classes.article}>
-                <section className={classes.aside1}>
+              <section className={classes.aside1}>
+                <Typography className={classes.instructions}>
+                  {getStepContent(activeStep, isNextClicked)}
+                </Typography>
 
-                
-                <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-                  {/* <Picker/>
+                <div>
+                  <Button
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    className={classes.backButton}
+                  >
+                    Back
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                  >
+                    {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                  </Button>
+                </div>
+              </section>
 
-                  <TextFieldComponent id="email" name="email" label="Email Address" type="email" autoComplete="email"/>
-                  <div>
-                    <Button
-                      disabled={activeStep === 0}
-                      onClick={handleBack}
-                      className={classes.backButton}
-                    >
-                      Back
-                    </Button>
-                    <Button variant="contained" color="primary" onClick={handleNext}>
-                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                    </Button>
-                  </div>
-                   */}
-
-
-
-
-
-
-
-
-
-                </section>
-
-
-
-
-
-
-
-
-
-
-
-
-                <section className={classes.aside2}>
-                
-                  <Typography variant="h4" align="center" component="h6" className={classes.heading}>
-                    Your Choosen Destination.
+              <section className={classes.aside2}>
+                <Typography
+                  variant="h4"
+                  align="center"
+                  component="h6"
+                  className={classes.heading}
+                >
+                  Your Choosen Destination.
+                </Typography>
+                <div className={classes.destination}>
+                  <img
+                    src={itineraryData.tripImage}
+                    alt="Mountains"
+                    width="350"
+                    height="200"
+                  />
+                  <Typography variant="h6" className={classes.heading}>
+                    {itineraryData.tripName}
                   </Typography>
-                  <div className={classes.destination}>
-                    <img src={itineraryData.tripImage} alt="Mountains" width="350" height="200"/>
-                    <Typography variant="h6" className={classes.heading}>
-                      {itineraryData.tripName}
-                    </Typography>
-                    <Typography variant="subtitle1" component="h6" className={classes.heading}>
-                      {itineraryData.tripHighLight}
-                    </Typography>
-                  </div>
-                </section>
-
+                  <Typography
+                    variant="subtitle1"
+                    component="h6"
+                    className={classes.heading}
+                  >
+                    {itineraryData.tripHighLight}
+                  </Typography>
+                </div>
+              </section>
             </section>
-
-
-
-
-
-
-
-
-
-
-
-            
           </div>
         )}
       </div>
